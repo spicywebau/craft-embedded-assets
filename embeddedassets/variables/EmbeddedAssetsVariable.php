298c6ec2b28e9e2e676949c92496430b58151690
Benjamin Fleming <benjfleming21@gmail.com>
2016-01-16 16:35:03 +1100

    Shorter, more descriptive template methods

--- embeddedassets/variables/EmbeddedAssetsVariable.php
+++ embeddedassets/variables/EmbeddedAssetsVariable.php
@@ -4,17 +4,17 @@ namespace Craft;
 
 class EmbeddedAssetsVariable
 {
-	public function isEmbeddedAsset(AssetFileModel $asset)
+	public function isEmbedded(AssetFileModel $asset)
 	{
 		return craft()->embeddedAssets->getEmbeddedAsset($asset) !== null;
 	}
 
-	public function getEmbeddedAsset(AssetFileModel $asset)
+	public function fromAsset(AssetFileModel $asset)
 	{
 		return craft()->embeddedAssets->getEmbeddedAsset($asset);
 	}
 
-	public function getEmbeddedAssets($assets, $indexBy = null)
+	public function fromAssets($assets, $indexBy = null)
 	{
 		return craft()->embeddedAssets->getEmbeddedAssets($assets, $indexBy);
 	}
