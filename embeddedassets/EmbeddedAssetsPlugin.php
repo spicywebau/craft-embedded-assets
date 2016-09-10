fe0d77dc8901abf6927ef519196faae2fd342794
Benjamin Fleming <benjfleming21@gmail.com>
2016-09-10 12:51:13 +1000

    Fixed issue with last commit

--- embeddedassets/EmbeddedAssetsPlugin.php
+++ embeddedassets/EmbeddedAssetsPlugin.php
@@ -100,7 +100,7 @@ class EmbeddedAssetsPlugin extends BasePlugin
 	 */
 	public static function getCacheKey()
 	{
-		return 'embeddedassets_thumbs_' . self::getVersion();
+		return 'embeddedassets-0.3.4_thumbs';
 	}
 
 	/**
