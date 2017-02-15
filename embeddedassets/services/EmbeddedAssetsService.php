f53a67da33998110ea496547b791756c7ac7d69e
Benjamin Fleming <benjfleming21@gmail.com>
2017-02-14 20:28:41 -0800

    Allow reading embed data from secure sites

--- embeddedassets/services/EmbeddedAssetsService.php
+++ embeddedassets/services/EmbeddedAssetsService.php
@@ -319,6 +319,7 @@ class EmbeddedAssetsService extends BaseApplicationComponent
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 			$data = curl_exec($ch);
 			$error = curl_error($ch);
