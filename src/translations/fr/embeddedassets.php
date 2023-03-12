ea1d9fed9a0524503977e3e26ae7d94d1504d7c2
Scandella Cédric <cedric@scandella.fr>
2023-03-12 15:51:48 +0100

    Add French translation

---
+++ src/translations/fr/embeddedassets.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'As a security measure embed codes will not be shown.' => 'Par mesure de sécurité, les codes d’intégration ne seront pas affichés.',
+    'Could not retrieve embed information.' => 'Impossible d’accéder à la ressource externe.',
+    'Embed' => 'Ressources externes',
+    'Image' => 'Image',
+    'Link' => 'Lien',
+    'Provider' => 'Source',
+    'Replace' => 'Remplacer',
+    'Rich' => 'Riche',
+    'This embedded asset cannot be saved.' => 'Impossible de sauvegarder cette ressource externe.',
+    'This information is coming from an untrusted source.' => 'Ces données proviennent d’une source non fiable.',
+    'Video' => 'Vidéo',
+    'Use YouTube nocookie?' => 'Utiliser YouTube nocookie?',
+    'Whether to force YouTube video iframes to use the `youtube-nocookie.com` domain.' => 'Obliger ou non les iframes des vidéos YouTube à utiliser le domaine `youtube-nocookie.com`',
+    'Disable tracking on Vimeo iframes?' => 'Désactiver le suivi des iframes Vimeo ?',
+    'Whether to force Vimeo video iframes to use the query parameter `dnt=1` to prevent tracking.' => 'Forcer ou non les iframes vidéo de Vimeo à utiliser le paramètre de requête `dnt=1` pour empêcher le suivi.',
+];
